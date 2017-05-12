dffc98f95cefbb07a8fd366c3ddcb2a18e1f5d16
Jazzy <fairy7823@gmail.com>
2017-05-12 08:53:02 -0700

    some processing

--- HackSim/src/Hacking/MainControl.java
+++ HackSim/src/Hacking/MainControl.java
@@ -6,20 +6,28 @@ public class MainControl {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		write("Hello agent\n");
-		pause(800);
-		write("Welcome to Hacking Simulator\n");
-		pause(1000);
-		write("Your task today is to destroy the infamous AI called Magpie\n");
-		pause(800);
-		write("Do you accept this mission?");
+		HackProcessor proc = new HackProcessor(input);
+		//write("Hello agent\n");
+		//pause(800);
+		//write("Welcome to Hacking Simulator\n");
+		//pause(1000);
+		//write("Your task today is to destroy the infamous AI called Magpie\n");
+		//pause(800);
+		write("Do you accept this mission? Please answer yes or no");
 		String userIn = input.nextLine();
 		if(userIn.equalsIgnoreCase("no")){
 			pause(800);
 			write("We'll see you next time, agent");
 		}
 		else{
-			write("Our sources have reported that a computer is running Magpie\nat the IP address ");//fill in address
+			String currIp = newIP();
+			String nextIp = newIP();
+			Server serv1 = new Server("admin", "password", nextIp, currIp, input, proc);
+			write("Our sources have reported that a computer is running Magpie\nat the IP address "+currIp);
+			userIn = input.nextLine();
+			proc.process(userIn, serv1);
+			userIn = input.nextLine();
+			proc.process(userIn, serv1);		
 		}
 	}
 	
@@ -37,5 +45,11 @@ public class MainControl {
 		}
 		System.out.println();
 	}
+	
+	public static String newIP(){
+		String ip = (int) (Math.random() * 256) +"."+(int) (Math.random() * 256) +"."+(int) (Math.random() * 256) +"."+(int) (Math.random() * 256);
+		return ip;
+	}
+
 
 }
--- HackSim/src/Hacking/Server.java
+++ HackSim/src/Hacking/Server.java
@@ -1,21 +1,30 @@
 package Hacking;
 
+import java.util.Scanner;
+
 public class Server {
 	
 	private String username;
 	private String password;
+	private String ipAddress;
+	
 	private String file;
+	
 	private boolean access;
 	private boolean found;
-	private int ipAddress;
 	
-	public Server(String user, String pass, String fileText, int ip){
+	private Scanner input;
+	private HackProcessor proc;
+	
+	public Server(String user, String pass, String fileText, String ip, Scanner in, HackProcessor process){
 		username = user;
 		password = pass;
 		file = fileText;
 		access = false;
 		found = false;
 		ipAddress = ip;
+		input = in;
+		proc = process;
 	}
 	
 	public void login(String user, String pass){
@@ -26,18 +35,32 @@ public class Server {
 		else if(user.equals(username)&&found){
 			MainControl.write("Incorrect username");
 		}
-		else if(pass.equals(password)&&found){
+		else if(user.equals(username)&&found){
 			MainControl.write("Incorrect password");
 		}
-		else if(found){
+		else if (found){
 			MainControl.write("Incorrect username and password");
 		}
 	}
 	
-	public void find(int ip){
-		if(ip == ipAddress){
+	public void find(String ip){
+		if(ipAddress.equals(ip)){
 			found = true;
+			MainControl.write("Server found");
 		}
+		else{
+			MainControl.write("Server not found, reenter command and IP");
+			String command = input.nextLine();
+			proc.process(command, this);
+		}
+	}
+	
+	public String getIp(){
+		return ipAddress;
+	}
+	
+	public boolean getFound(){
+		return found;
 	}
 	
 }
